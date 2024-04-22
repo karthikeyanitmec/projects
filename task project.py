e2d53694f55b59317b112dd3f9cfa11a880700bb
karthkeyanitmec <vijikarthi63840@gmail.com>
2024-04-22 11:32:40 -0700

    hello

---
+++ task project.py
@@ -0,0 +1,90 @@
+# # #ATM PROJECT _______________________________________________________
+
+
+# # account={
+# #     "holdername":"xxxxxx",
+# #     "balance":10000
+# # }
+# # pin=int(input("Enter your six digit pin number:"))
+# # if (pin>100000):
+# #   process=input("choose the withdraw or deposit or balancecheck:")
+# #   if (process=="withdraw" or process=="WITHDRAW"):
+# #        withdrawmoney=int(input("enter your withdraw amount:"))
+# #        account['balance']-=withdrawmoney
+# #        print(withdrawmoney,"is withdraw","balance:",account['balance'])
+# #        print("please wait..collect your money")
+# #   elif(process=="deposit" or process=="DEPOSIT"):
+# #       depositamt=int(input("enter your deposit amount:"))
+# #       account['balance']+=depositamt
+# #       print(depositamt,"is deposited.","balance is ",account['balance'])
+# #       print("amount deposited")
+# #   elif(process=="balancecheck" or process=="BALANCECHECK"):
+# #       print("balance:",account['balance'])
+# #   else:
+# #       print("choose the correct option") 
+# # else:
+# #      print("please enter your correct pin number")
+
+
+
+
+# # #Task theme park_______________________________________
+
+
+# print("_________________WELCOME TO THEME PARK___________________")
+# customername=input("Enter your name:")
+# age=int(input("Enter your age:"))
+# if((age<=18)and(age>5)):
+#     print("You will play a kids games")
+#     a=1
+#     while(a<=1):
+#       games=input("What games are you play.Available games are waterfall and skidding games:")
+#       if (games=="waterfall" or games=="skidding" ):
+#            if (games=="waterfall"):
+#                 print("Your zone no:1")
+#                 break
+#            else:
+#                 print("your zone no:2")
+#                 break
+
+#       elif(games=="waterfall,skidding" or games=="skidding,waterfall"):
+#             if (games=="waterfall,skidding"):
+#                 print("Your zone no:1 and 2")
+#                 break
+#             else:
+#                 print("your zone no:2 and 1")
+#                 break
+#       else:
+#             print("choose only available games:")
+# elif(age>18 and age<=40):
+#     print("You will play a thrilling games ")
+#     i=1
+#     while(i<=1):
+#         games=input("What games are you play.Available games are coloumbous and trains games:")
+#         if(games=="coloumbous" or games=="trains"):
+#            if (games=="coloumbous"):
+#                print("Your zone no:3")
+#                break
+#            else:
+#                 print("your zone no:4")
+#                 break
+#         elif(games=="coloumbous,trains" or games=="trains,coloumbous"):
+#            if (games=="coloumbous,trains"):
+#                 print("Your zone no:3 and 4")
+#                 break
+#            else:
+#                 print("your zone no:4 and 3")
+#                 break
+#         else:
+#            print("choose only available games:")
+# elif(age>40 and age<=60):
+#     print("You will play a fun games ")
+#     print("Then,only you will play music chair games")
+#     print("Your zone no:6 ")
+# else:
+#     print("Sorry,you are not eligible for theme park")
+
+
+
+# _______________________Bus Reservation____________________
+
